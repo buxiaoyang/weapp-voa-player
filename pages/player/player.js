9f3cc5b2eb49e0aa07262732654620d82ef1d0b6
Xiao-Yang Bu <xiaoyang.bu@hpe.com>
2018-07-10 14:25:13 +0800

    Get api data ready

--- pages/player/player.js
+++ pages/player/player.js
@@ -1,25 +1,73 @@
 
 Page({
-  data:{
-    ctrl: "音乐控制区域",
-    cpTime: '00:00',
-    duration: '00:00',
-    lrcList: []
-  },
-  onLoad:function(param) {
-  
-  },
-  onReady:function(){
- 
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-    api.playCtrl.pause()
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+    data:{
+        cpTime: '10:00',
+        duration: '00:00',
+        items: [],
+        itemIndex: -1,
+    },
+
+    onLoad:function(param) {
+        this.loadData();
+        // wx.playBackgroundAudio({
+        //     dataUrl: 'http://downdb.51voa.com/201806/mattis-nato-defense-ministerial.mp3',
+        //     title: 'VOA'      
+        // })
+    },
+
+    loadData: function()
+    {
+        var that = this;
+        wx.showLoading({
+            title: '加载中',
+        })
+        wx.request({
+            url: 'https://weapp.euleriii.cn/data.json',
+            success: function (res) {
+                wx.hideLoading();
+                console.log(res);
+                if (res.statusCode != 200) {
+                    wx.showModal({
+                        title: '发生错误',
+                        content: '请求资源失败，状态码：' + res.statusCode,
+                        showCancel: false,
+                        confirmText: "重新加载",
+                        success: function (res) {
+                            if (res.confirm) {
+                                that.loadData();
+                            }
+                        }
+                    })
+                }
+                else
+                {
+                    that.setData({ items: res.data.items });
+                    that.buildList();
+                }
+            },
+            fail: function (error) {
+                wx.hideLoading();
+                console.log(error)
+                wx.showModal({
+                    title: '发生错误',
+                    content: '请求资源失败',
+                    showCancel: false,
+                    confirmText: "重新加载",
+                    success: function (res) {
+                        if (res.confirm) {
+                            that.loadData();
+                        }
+                    }
+                })
+            },
+        })
+    },
+
+    buildList: function(){
+        var that = this;
+        if (that.data.items && that.data.items.length > 0)
+        {
+            that.setData({ itemIndex: 0});
+        }
+    },
 })
